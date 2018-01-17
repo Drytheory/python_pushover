713f7110fa824281dab89c87e3472498f282902f
Drytheory <35511480+Drytheory@users.noreply.github.com>
2018-01-18 00:09:31 +0100

    Add files via upload

--- example.py
+++ example.py
@@ -1,2 +1,2 @@
 from push import send_push_message
-send_push_message("APPLICATION_KEY", "DEIN TITEL", "DEINE NACHRICHT")
+send_push_message("ax581io5o1f1iire3yiyrscux87ync", "DEIN TITEL", "DEINE NACHRICHT")
--- push.py
+++ push.py
@@ -1,7 +1,17 @@
 import requests
 
-user_token = 'USER_KEY'
+
+#Hier den User Key eintragen
+user_token = 'unej1vrdhj91xvz6o4q88i3aair6bq'
+
+
 
 def send_push_message(app_key, title, message):
-	push_data = {'token': app_key, 'user': user_token, 'title': title, 'message': message}
+	push_data = {
+		'token': app_key, 
+		'user': user_token, 
+		'title': title, 
+		'message': message
+	}
+
 	post_request = requests.post('https://api.pushover.net/1/messages.json', data=push_data)
